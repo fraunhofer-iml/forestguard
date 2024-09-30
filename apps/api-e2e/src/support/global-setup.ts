81e4e34be571744a63921ddf4bf3749d25705b8d
Antonius Paul <antonius.paul@iml.fraunhofer.de>
2024-09-30 10:33:42 +0000

    feat(test): add integration tests for plot of land
    Closes FOR-399

--- apps/api-e2e/src/support/global-setup.ts
+++ apps/api-e2e/src/support/global-setup.ts
@@ -7,7 +7,7 @@ module.exports = async function () {
   // Start services that that the app needs to run (e.g. database, docker-compose, etc.).
   console.log('\nSetting up...');
 
-  console.log(' -> database with Prisma\n');
+  console.log(' -> database schema without data\n');
   execSync('npx prisma db push --force-reset --schema=../../prisma/schema.prisma');
 
   // Hint: Use `globalThis` to pass variables to global teardown.
