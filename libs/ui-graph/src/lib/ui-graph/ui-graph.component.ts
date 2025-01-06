133809a120c222b1e4a98dc3a228399c768689c8
Matthias Schönborn <matthias.schoenborn@iml.fraunhofer.de>
2025-01-06 16:01:16 +0100

    Switch icons from material font to svgs

--- libs/ui-graph/src/lib/ui-graph/ui-graph.component.ts
+++ libs/ui-graph/src/lib/ui-graph/ui-graph.component.ts
@@ -13,7 +13,7 @@ export class UiGraphComponent implements OnInit, OnChanges {
   @Input() height = 600;
   @Input() selectedNode?: string;
   @Input() invalidEdges: Edge[] | null = [];
-  @Input() transitionLength = 300;
+  @Input() transitionLength = 200;
   @Input() nodesWithEUInfoSystemId: string[] | null = [];
   @Input() nodesWithProcessDocuments: string[] | null = [];
 
@@ -36,6 +36,7 @@ export class UiGraphComponent implements OnInit, OnChanges {
   private container?: Selection<SVGGElement, unknown, null, undefined>;
   private links?: Selection<SVGGElement, unknown, null, undefined>;
   private nodes?: Selection<SVGGElement, unknown, null, undefined>;
+  private icons?: Selection<SVGGElement, unknown, null, undefined>;
   private sankey?: any;
   private zoomFn?: any;
   private showTimeout: number | null = null;
@@ -47,7 +48,14 @@ export class UiGraphComponent implements OnInit, OnChanges {
   private readonly euInfoSystemTooltipText = 'EU System Info ID available';
   private readonly processDocumentsTooltipText = 'Process-specific documents available';
   private readonly batchInformationTooltipTemplate = (data: any): string =>
-    `<strong>Batch ${data.id}</strong>\nProcess:\t\t\t\t\t${data.name}\nWeight [kg]:\t\t\t${data.weight}\nDate of Process:\t${new Date(data.processStepDateOfProcess).toLocaleString()}`;
+    `<strong>Batch ${data.id}</strong>\nProcess:\t\t\t\t${data.name}\nWeight [kg]:\t\t\t${data.weight}\nDate of Process:\t${new Date(data.processStepDateOfProcess).toLocaleString()}`;
+
+  private readonly iconSize = 48;
+  private readonly euInfoIconPath =
+    'M481-781q106 0 200 45.5T838-604q7 9 4.5 16t-8.5 12q-6 5-14 4.5t-14-8.5q-55-78-141.5-119.5T481-741q-97 0-182 41.5T158-580q-6 9-14 10t-14-4q-7-5-8.5-12.5T126-602q62-85 155.5-132T481-781Zm0 94q135 0 232 90t97 223q0 50-35.5 83.5T688-257q-51 0-87.5-33.5T564-374q0-33-24.5-55.5T481-452q-34 0-58.5 22.5T398-374q0 97 57.5 162T604-121q9 3 12 10t1 15q-2 7-8 12t-15 3q-104-26-170-103.5T358-374q0-50 36-84t87-34q51 0 87 34t36 84q0 33 25 55.5t59 22.5q34 0 58-22.5t24-55.5q0-116-85-195t-203-79q-118 0-203 79t-85 194q0 24 4.5 60t21.5 84q3 9-.5 16T208-205q-8 3-15.5-.5T182-217q-15-39-21.5-77.5T154-374q0-133 96.5-223T481-687Zm0-192q64 0 125 15.5T724-819q9 5 10.5 12t-1.5 14q-3 7-10 11t-17-1q-53-27-109.5-41.5T481-839q-58 0-114 13.5T260-783q-8 5-16 2.5T232-791q-4-8-2-14.5t10-11.5q56-30 117-46t124-16Zm0 289q93 0 160 62.5T708-374q0 9-5.5 14.5T688-354q-8 0-14-5.5t-6-14.5q0-75-55.5-125.5T481-550q-76 0-130.5 50.5T296-374q0 81 28 137.5T406-123q6 6 6 14t-6 14q-6 6-14 6t-14-6q-59-62-90.5-126.5T256-374q0-91 66-153.5T481-590Zm-1 196q9 0 14.5 6t5.5 14q0 75 54 123t126 48q6 0 17-1t23-3q9-2 15.5 2.5T744-191q2 8-3 14t-13 8q-18 5-31.5 5.5t-16.5.5q-89 0-154.5-60T460-374q0-8 5.5-14t14.5-6Z';
+
+  private readonly documentsIconPath =
+    'M320-240h320v-80H320v80Zm0-160h320v-80H320v80ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z';
 
   constructor(private _element: ElementRef) {}
 
@@ -76,6 +84,7 @@ export class UiGraphComponent implements OnInit, OnChanges {
     this.container = this.svg.append('g').attr('class', 'container');
     this.links = this.container.append('g').attr('class', 'links').attr('class', 'dependencyGraphElement');
     this.nodes = this.container.append('g').attr('class', 'nodes').attr('class', 'dependencyGraphElement');
+    this.icons = this.container.append('g').attr('class', 'icons').attr('class', 'dependencyGraphElement');
 
     this.update();
   }
@@ -94,7 +103,7 @@ export class UiGraphComponent implements OnInit, OnChanges {
     }
   }
 
-  update(): void {
+  async update(): Promise<void> {
     const invalidLinkSet = new Set(this.invalidEdges?.map((edge) => `${edge.from}${edge.to}`));
     const getFillColor = (d: any) => {
       const isSelectedNode = d.id === this.selectedNode;
@@ -107,7 +116,7 @@ export class UiGraphComponent implements OnInit, OnChanges {
       return isSelectedNode ? this.selectedNodeColor : this.validNodeColor;
     };
 
-    if (!this.svg || !this.container || !this.links || !this.nodes || !this.data || !this.margin) {
+    if (!this.svg || !this.container || !this.links || !this.nodes || !this.data || !this.margin || !this.icons) {
       return console.error('Missing svg, container, links or nodes');
     }
 
@@ -260,13 +269,13 @@ export class UiGraphComponent implements OnInit, OnChanges {
             .attr('fill', (d) => (d.id === this.selectedNode ? this.selectedNodeTextColor : this.nonSelectedNodeTextColor))
             .attr('font-weight', '500')
             .attr('text-anchor', 'middle')
-            .attr('x', (d) => d.x0 + (d.x1 - d.x0) / 2 || -75)
-            .attr('y', (d) => d.y0 + (d.y1 - d.y0) / 2 || 25)
+            .attr('x', (d) => d.x0 + (d.x1 - d.x0) / 2)
+            .attr('y', (d) => d.y0 + (d.y1 - d.y0) / 2)
             .attr('dy', '0.25em')
             .attr('cursor', 'pointer')
             .attr('transform', (d) => {
-              const x = d.x0 + (d.x1 - d.x0) / 2 || 100;
-              const y = d.y0 + (d.y1 - d.y0) / 2 || 100;
+              const x = d.x0 + (d.x1 - d.x0) / 2;
+              const y = d.y0 + (d.y1 - d.y0) / 2;
               return `rotate(270, ${x}, ${y})`;
             }),
         (update) =>
@@ -276,14 +285,14 @@ export class UiGraphComponent implements OnInit, OnChanges {
             .ease(easeQuadInOut)
             .attr('fill', (d) => (d.id === this.selectedNode ? this.selectedNodeTextColor : this.nonSelectedNodeTextColor))
             .attr('x', (d) => {
-              return d.x0 + (d.x1 - d.x0) / 2 || -this.height / 2;
+              return d.x0 + (d.x1 - d.x0) / 2;
             })
             .attr('y', (d) => {
-              return d.y0 + (d.y1 - d.y0) / 2 || 25;
+              return d.y0 + (d.y1 - d.y0) / 2;
             })
             .attr('transform', (d) => {
-              const x = d.x0 + (d.x1 - d.x0) / 2 || 0;
-              const y = d.y0 + (d.y1 - d.y0) / 2 || 0;
+              const x = d.x0 + (d.x1 - d.x0) / 2;
+              const y = d.y0 + (d.y1 - d.y0) / 2;
               return `rotate(270, ${x}, ${y})`;
             })
       )
@@ -318,7 +327,7 @@ export class UiGraphComponent implements OnInit, OnChanges {
   }
 
   private addVisualIndicators() {
-    if (!this.svg || !this.container || !this.links || !this.nodes || !this.data || !this.margin) {
+    if (!this.svg || !this.container || !this.links || !this.nodes || !this.data || !this.margin || !this.icons) {
       return console.error('Missing svg, container, links or nodes');
     }
 
@@ -349,19 +358,18 @@ export class UiGraphComponent implements OnInit, OnChanges {
       .style('backdrop-filter', 'blur(5px)');
 
     this.nodes
-      .selectAll('text.icon-eu-info-system')
+      .selectAll('svg.icon-eu-info-system-hover')
       .data(this.data.nodes, (d: any) => d.id)
       .join(
         (enter) =>
           enter
-            .append('text')
-            .filter((d) => this.nodesWithEUInfoSystemId?.includes(d.id) || false)
-            .attr('class', 'icon-eu-info-system')
+            .filter((d) => this.nodesWithProcessDocuments?.includes(d.id) || false)
+            .append('rect')
             .attr('x', (d) => d.x0 + 72)
-            .attr('y', (d) => d.y0 - 52)
-            .attr('font-family', 'Material Symbols Outlined')
-            .attr('font-size', '48px')
-            .text(this.getIconUnicode('fingerprint'))
+            .attr('y', (d) => d.y0 - 72)
+            .attr('width', this.iconSize)
+            .attr('height', this.iconSize)
+            .attr('fill', 'transparent')
             .on('mouseover', (event, data) =>
               this.onMouseOverElement('tooltip-icon-eu-info-system', this.euInfoSystemTooltipText, event, data)
             )
@@ -373,25 +381,55 @@ export class UiGraphComponent implements OnInit, OnChanges {
             .duration(this.transitionLength)
             .ease(easeQuadInOut)
             .attr('x', (d) => d.x0 + 72)
-            .attr('y', (d) => d.y0 - 52)
-            .text(this.getIconUnicode('fingerprint')),
+            .attr('y', (d) => (this.nodesWithEUInfoSystemId?.includes(d.id) ? d.y0 - 22 : d.y0 - 72)),
         (exit) => exit.remove()
       );
 
     this.nodes
-      .selectAll('text.icon-process-documents')
+      .selectAll('svg.icon-eu-info-system')
       .data(this.data.nodes, (d: any) => d.id)
       .join(
         (enter) =>
           enter
-            .append('text')
-            .filter((d) => this.nodesWithProcessDocuments?.includes(d.id) || false)
-            .attr('class', 'icon-process-documents')
+            .filter((d) => this.nodesWithEUInfoSystemId?.includes(d.id) || false)
+            .append('svg')
+            .attr('class', 'icon-eu-info-system')
             .attr('x', (d) => d.x0 + 72)
-            .attr('y', (d) => (this.nodesWithEUInfoSystemId?.includes(d.id) ? d.y0 + 0 : d.y0 - 52))
-            .attr('font-family', 'Material Symbols Outlined')
-            .attr('font-size', '48px')
-            .text(this.getIconUnicode('description'))
+            .attr('y', (d) => d.y0 - 72)
+            .attr('width', this.iconSize)
+            .attr('height', this.iconSize)
+            .attr('fill', '#000')
+            .attr('viewBox', '0 -960 960 960')
+            .append('path')
+            .attr('d', this.euInfoIconPath)
+            .on('mouseover', (event, data) =>
+              this.onMouseOverElement('tooltip-icon-eu-info-system', this.euInfoSystemTooltipText, event, data)
+            )
+            .on('mousemove', (event, data) => this.onMouseMoveElement('tooltip-icon-eu-info-system', event, data))
+            .on('mouseout', (event, data) => this.onMouseOutElement('tooltip-icon-eu-info-system', event, data)),
+        (update) =>
+          update
+            .transition()
+            .duration(this.transitionLength)
+            .ease(easeQuadInOut)
+            .attr('x', (d) => d.x0 + 72)
+            .attr('y', (d) => d.y0 - 72),
+        (exit) => exit.remove()
+      );
+
+    this.nodes
+      .selectAll('svg.icon-process-documents-hover')
+      .data(this.data.nodes, (d: any) => d.id)
+      .join(
+        (enter) =>
+          enter
+            .filter((d) => this.nodesWithProcessDocuments?.includes(d.id) || false)
+            .append('rect')
+            .attr('x', (d) => d.x0 + 72)
+            .attr('y', (d) => (this.nodesWithEUInfoSystemId?.includes(d.id) ? d.y0 - 22 : d.y0 - 72))
+            .attr('width', this.iconSize)
+            .attr('height', this.iconSize)
+            .attr('fill', 'transparent')
             .on('mouseover', (event, data) =>
               this.onMouseOverElement('tooltip-icon-process-documents', this.processDocumentsTooltipText, event, data)
             )
@@ -403,8 +441,44 @@ export class UiGraphComponent implements OnInit, OnChanges {
             .duration(this.transitionLength)
             .ease(easeQuadInOut)
             .attr('x', (d) => d.x0 + 72)
-            .attr('y', (d) => (this.nodesWithEUInfoSystemId?.includes(d.id) ? d.y0 + 0 : d.y0 - 52))
-            .text(this.getIconUnicode('description')),
+            .attr('y', (d) => (this.nodesWithEUInfoSystemId?.includes(d.id) ? d.y0 - 22 : d.y0 - 72)),
+        (exit) => exit.remove()
+      );
+
+    // <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M320-240h320v-80H320v80Zm0-160h320v-80H320v80ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z"/></svg>
+    this.nodes
+      .selectAll('svg.icon-process-documents')
+      .data(this.data.nodes, (d: any) => d.id)
+      .join(
+        (enter) =>
+          enter
+            .filter((d) => this.nodesWithProcessDocuments?.includes(d.id) || false)
+            .append('svg')
+            .attr('xmlns', 'http://www.w3.org/2000/svg')
+            .attr('x', (d) => d.x0 + 72)
+            .attr('y', (d) => d.y0 - 22)
+            // .attr('x', (d) => d.x0 + 72)
+            // .attr('y', (d) => (this.nodesWithEUInfoSystemId?.includes(d.id) ? d.y0 - 22 : d.y0 - 72))
+            .attr('class', 'icon-process-documents')
+            .attr('width', this.iconSize)
+            .attr('height', this.iconSize)
+            .on('mouseover', (event, data) =>
+              this.onMouseOverElement('tooltip-icon-process-documents', this.processDocumentsTooltipText, event, data)
+            )
+            .on('mousemove', (event, data) => this.onMouseMoveElement('tooltip-icon-process-documents', event, data))
+            .on('mouseout', (event, data) => this.onMouseOutElement('tooltip-icon-process-documents', event, data))
+            .attr('viewBox', '0 -960 960 960')
+            .append('path')
+            .attr('d', this.documentsIconPath)
+            .attr('fill', '#000'),
+
+        (update) =>
+          update
+            .transition()
+            .duration(this.transitionLength)
+            .ease(easeQuadInOut)
+            .attr('x', (d) => d.x0 + 72)
+            .attr('y', (d) => (this.nodesWithEUInfoSystemId?.includes(d.id) ? d.y0 - 22 : d.y0 - 72)),
         (exit) => exit.remove()
       );
   }
