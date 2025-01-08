30b99591e9376edf43e94ccba3b0030889911c65
Matthias Schönborn <matthias.schoenborn@iml.fraunhofer.de>
2025-01-08 14:56:17 +0100

    Add status information field

--- apps/process-svc/src/app/batch/read/batch-read.service.ts
+++ apps/process-svc/src/app/batch/read/batch-read.service.ts
@@ -15,6 +15,25 @@ export class BatchReadService {
 
   async readBatchesByCompanyId(companyId: string, query: string, sorting: string): Promise<BatchDto[]> {
     const batches = await this.prismaService.batch.findMany(readCoffeeBatchesByCompanyIdQuery(companyId, query, sorting));
-    return batches.map(mapBatchPrismaToBatchDto);
+    const batchDtos = batches.map(mapBatchPrismaToBatchDto);
+    for (const batch of batchDtos) {
+      batch.hasAllProofs = await this.getStatusOfBatch(batch.id);
+      console.log(batch.hasAllProofs);
+    }
+    return batchDtos;
+  }
+
+  async getStatusOfBatch(batchId: string): Promise<boolean> {
+    const batch = await this.prismaService.batch.findUniqueOrThrow(readBatchByIdQuery(batchId));
+
+    if (batch.processStep.farmedLand) {
+      return batch.processStep.farmedLand.proofs?.length === 2;
+    }
+
+    if (!batch.processStep.farmedLand && batch.ins.length > 0) {
+      return batch.ins.every((previousBatch) => this.getStatusOfBatch(previousBatch.id));
+    }
+
+    return false;
   }
 }
