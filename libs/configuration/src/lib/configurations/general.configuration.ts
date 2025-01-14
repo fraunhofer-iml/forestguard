87fd4d3981181773bac553a2d5beda9240e62583
Michael Pichura <michael.pichura@iml.fraunhofer.de>
2025-01-14 10:26:07 +0100

    hotfix(configuration): use old envs for minio

--- libs/configuration/src/lib/configurations/general.configuration.ts
+++ libs/configuration/src/lib/configurations/general.configuration.ts
@@ -27,8 +27,8 @@ export default registerAs(GENERAL_CONFIG_IDENTIFIER, () => ({
     endPoint: process.env['MINIO_ENDPOINT'] || 'localhost',
     port: parseInt(process.env['MINIO_PORT'] || '9000'),
     useSSL: process.env['MINIO_USE_SSL'] === 'true',
-    accessKey: process.env['MINIO_ROOT_USER'] || 'admin',
-    secretKey: process.env['MINIO_ROOT_PASSWORD'] || 'blockchain',
+    accessKey: process.env['MINIO_ACCESS_KEY'] || 'admin',
+    secretKey: process.env['MINIO_SECRET_KEY'] || 'blockchain',
     bucketName: process.env['MINIO_BUCKET_NAME'] || 'forest-guard',
   },
 }));
