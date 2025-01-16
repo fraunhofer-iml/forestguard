d28f4406a8ba73b0dcef9ab6c476ba79a92bcea1
Michael Pichura <michael.pichura@iml.fraunhofer.de>
2025-01-16 11:12:31 +0100

    Remove unused constant

--- apps/frontend/src/app/shared/messages.ts
+++ apps/frontend/src/app/shared/messages.ts
@@ -17,7 +17,6 @@ export enum Messages {
   successMasterDataImport = 'Data successfully imported',
   successUpdateFarmer = 'Farmer successfully updated',
   error = 'Please fill in all required fields',
-  errorCreateHarvest = 'Creation of harvest batches failed.',
   invalidGeoData = 'Invalid JSON format.',
   errorCompany = 'Please add a company before proceeding',
   errorMasterDataImport = 'Invalid File',
